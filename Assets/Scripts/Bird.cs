81f1cc8d91e09ac563567b67fc402378354009d3
Stefan <bozinovski.stefan@gmail.com>
2021-03-22 16:11:11 +0100

    Initial commit

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bird : MonoBehaviour
+{   
+    private Vector3 _initialPosition;
+    private bool _birdWasLaunched;
+    private float _timeSittingAround;
+    [SerializeField]private int launchSpeed = 100;
+    
+    
+    private void Awake() 
+    {
+        _initialPosition = transform.position;
+    }
+
+    private void Update() 
+    {
+        GetComponent<LineRenderer>().SetPosition(0, transform.position);
+        GetComponent<LineRenderer>().SetPosition(1, _initialPosition);
+
+        if (_birdWasLaunched &&
+            GetComponent<Rigidbody2D>().velocity.magnitude <= 0.1)
+            {
+                _timeSittingAround += Time.deltaTime;
+            }
+
+        if (transform.position.y > 10 ||
+            transform.position.y < -10 ||
+            transform.position.x > 20 ||
+            transform.position.x < -20 ||
+            _timeSittingAround > 3)
+        {
+            string currentSceneName = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(currentSceneName);
+        }
+    }
+
+    private void OnMouseDown() 
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+        GetComponent<LineRenderer>().enabled = true;
+    }
+
+    private void OnMouseUp() 
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+
+        Vector2 distanceToInitialPosition = _initialPosition - transform.position;
+        GetComponent<Rigidbody2D>().AddForce(distanceToInitialPosition * launchSpeed);
+        GetComponent<Rigidbody2D>().gravityScale = 1;
+        _birdWasLaunched = true;
+        GetComponent<LineRenderer>().enabled = false;
+    }
+
+    private void OnMouseDrag() 
+    {
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(newPosition.x, newPosition.y);
+    }
+}
